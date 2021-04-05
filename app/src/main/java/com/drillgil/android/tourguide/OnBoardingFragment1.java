31125fb2e2be8b4639189cc4834c736f12d863f5
galagl12345 <almuly.gil212@gmail.com>
2021-04-05 10:59:48 +0300

    Initial commit

---
+++ app/src/main/java/com/drillgil/android/tourguide/OnBoardingFragment1.java
@@ -0,0 +1,26 @@
+package com.drillgil.android.tourguide;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class OnBoardingFragment1 extends Fragment {
+
+    public OnBoardingFragment1(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding1, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/drillgil/android/tourguide/OnBoardingFragment2.java
@@ -0,0 +1,25 @@
+package com.drillgil.android.tourguide;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class OnBoardingFragment2 extends Fragment {
+
+    public OnBoardingFragment2(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding2, container, false);
+
+        return root;    }
+}
